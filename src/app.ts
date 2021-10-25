7e35231258fb2960aafb07f3d6a91730ca65a4e1
cherryvue <yetiasg@gmail.com>
2021-10-25 21:30:20 +0200

    add models, interfaces, config.ts and some small changes

--- src/app.ts
+++ src/app.ts
@@ -3,9 +3,13 @@ import experss, { Request, Response } from 'express'
 const app = experss()
 
 app.get('/', (req: Request, res: Response) => {
-  res.status(200).json({message: "rootRoute"})
+  res.status(200).json({message: "rootRoutes"})
 })
 
-app.listen(5000, () => {
-  console.log(`Listening on port: 5000`)
+app.get('/u', (req: Request, res: Response) => {
+  res.status(200).json({message: "uRoutes"})
+})
+
+app.listen(process.env.PORT, () => {
+  console.log(`Listening on port: ${process.env.PORT}`)
 })
